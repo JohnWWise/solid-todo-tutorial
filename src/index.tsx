8f386b02384bfdaba5a00fc193a9ad3418a99189
JohnWWise <jwalterwi@gmail.com>
2025-06-02 14:04:38 -0400

    Corrected root null warning in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { SessionProvider } from "@inrupt/solid-ui-react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <SessionProvider>
     <App />
